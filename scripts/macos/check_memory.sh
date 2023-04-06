6ba7c905189c4154f371772a717b9e04cbefad0c
Patryk <8299306+kpatryk@users.noreply.github.com>
2023-04-06 08:42:16 +0200

    feat(scripts): Add git scripts

---
+++ scripts/macos/check_memory.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function parse_mem() {
+    name=$1
+    vm_stat | awk "/$name:/"'{print $NF}' | tr -d '.'
+}
+
+FREE_BLOCKS=$(parse_mem free)
+ACTIVE_BLOCKS=$(parse_mem "Pages active")
+INACTIVE_BLOCKS=$(parse_mem inactive)
+SPECULATIVE_BLOCKS=$(parse_mem speculative)
+
+FREE=$((($FREE_BLOCKS + $SPECULATIVE_BLOCKS) * 4096 / 1048576))
+INACTIVE=$(($INACTIVE_BLOCKS * 4096 / 1048576))
+ACTIVE=$(($ACTIVE_BLOCKS * 4096 / 1048576))
+TOTAL=$(($FREE + $INACTIVE))
+
+echo Free:       $FREE MB
+echo Active:     $ACTIVE MB
+echo Inactive:   $INACTIVE MB
+echo Total free: $TOTAL MB
