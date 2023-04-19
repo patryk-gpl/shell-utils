420602289c704fbe4f0498d13ecaac8b7396715b
Patryk Kubiak <8299306+kpatryk@users.noreply.github.com>
2023-04-19 13:41:24 +0200

    refactor: Move some of the scripts code to functions

---
+++ functions/general/git.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+####################################################################################################
+# This file contains functions to work with Git repositories
+####################################################################################################
+
+# This function will show the local and remote branch head commit hashes.
+git_current_branch_heads() {
+  echo "Local branch head: $(git rev-parse --verify HEAD)"
+  echo "Remote branch head: $(git rev-parse --verify 'HEAD@{upstream}')"
+}
+
+# Search for a pattern in the Git history
+git_history_search() {
+  [ -z "$1" ] && {
+    echo "Missing pattern. Aborting.."
+    return
+  } || pattern="$1"
+
+  git log --all -S"$pattern" --oneline |
+    while read -r commit; do git show "${commit:0:7}"; done
+}
+
+# Search for a pattern in all commits, show the commit hash and the line number
+git_grep_all_commits() {
+  [ -z "$1" ] && {
+    echo "Missing pattern. Aborting.."
+    return
+  } || pattern="$1"
+
+  echo "Checking Git history for pattern: $pattern"
+  mapfile -t commits < <(git rev-list --all)
+  git grep -n "$pattern" "${commits[@]}"
+}
+
+# Show folder name and its size, sort the result
+git_folder_size() {
+  git_dir=${1:-"."}
+  [ ! -d "$git_dir" ] || [ ! -f "$git_dir/.git" ] && {
+    echo "Source directory is missing or not a Git folder. Aborting.."
+    return 1
+  }
+
+  for dir in "$git_dir"/*; do
+    if [ -d "$dir" ]; then
+      if [ -d "$dir/.git" ]; then
+        size=$(du -sh "$dir" | awk '{print $1}')
+        echo "$size - $dir"
+        if [ -f "$dir/.meta" ]; then
+          git_folder_size "$dir"
+        fi
+      else
+        git_folder_size "$dir"
+      fi
+    fi
+  done | sort -h
+}
+
+# Clone a Git repository with depth 1 (shallow clone)
+git_clone_shallow() {
+  [ -z "$1" ] && {
+    echo "Missing URL. Aborting.." >&2
+    return 1
+  } || url="$1"
+
+  if [ -z "$2" ]; then
+    git clone --depth 1 "$url"
+  else
+    git clone --depth 1 "$url" "$2"
+  fi
+
+}
+
+# Run 'git gc' command on all Git repositories recursively, starting from the current directory if no argument is provided
+git_gc_recursively() {
+  dir=${1:-.}
+  echo "Running 'git gc' command on directories containing '.git' starting from $dir recursively"
+  find "$dir" -type d -name ".git" -execdir git gc {} \;
+}
+
+export -f git_clone_shallow
--- scripts/helper_python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Upgrade all packages installed in Python virtualenv
-function virtualenv_all_pkg_upgrade() {
-    for pkg in $(pip list --outdated --format=freeze | awk -F"=" '/^[a-z]/ {print $1}')
-    do
-        pip install --upgrade "$pkg"
-    done
-}
