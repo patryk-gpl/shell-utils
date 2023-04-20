2d2f94dfc46561863eaa61399af42c183b995d8f
Patryk <8299306+kpatryk@users.noreply.github.com>
2023-04-20 20:35:15 +0200

    feat(kubernetes): Add functions relevant to work with K8S clusters

---
+++ functions/containers/kubernetes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Show the current namespace name
+kube_ns_current() {
+  kubectl config view --minify --output 'jsonpath={..namespace}'
+}
+
+# List all images used in the current namespace
+kube_image_list_names_from_pods() {
+  namespace=${1:-$(kube_ns_current)}
+
+  kubectl get pods -n "$namespace" -o=jsonpath="{range .items[*].spec.containers[*]}{.image}{'\n'}{end}" | sort -u
+}
+
+# List all images used in all namespaces
+kube_image_list_names_from_pods_all_namespaces() {
+  kubectl get pods -A -o=jsonpath="{range .items[*].spec.containers[*]}{.image}{'\n'}{end}" | sort -u
+}
