033bbae826c2ca80b4f3479ac389058cc255220b
Patryk <8299306+kpatryk@users.noreply.github.com>
2023-06-09 21:12:18 +0200

    feat(docker): Scan all local Docker images for existing CVEs

---
+++ scripts/docker/scan_all_local_images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+images=$(docker images --format "{{.Repository}}:{{.Tag}}")
+
+for image in $images; do
+  filename=${image//\//_}
+  echo "Checking CVEs for image: $image. Save report to $filename.cve"
+  docker scout cves "$image" > "$filename.cve"
+done
