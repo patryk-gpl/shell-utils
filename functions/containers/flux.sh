1a03b52b5acc760f20ab004565064a1d05de8941
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2023-07-04 19:49:39 +0200

    feat(flux): Show all Flux v2 active resources in the given namespace

---
+++ functions/containers/flux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+####################################################################################################
+# This file contains functions to work with Flux v2
+####################################################################################################
+
+source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
+prevent_to_execute_directly
+
+flux_list_all_active_resources() {
+  local flux_resources
+  local namespace="$1"
+  flux_resources=$(kubectl api-resources --verbs=list -n "$namespace" -o name | awk '/flux/' | paste -sd "," -)
+
+  kubectl get "$flux_resources" -n "$namespace"
+}
