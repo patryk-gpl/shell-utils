10a7bb3f9ce61585d04f21294003ce330f3870b5
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2023-08-07 13:04:48 +0200

    fix: Make source work on both ZSH and BASH

--- functions/general/filesystem.sh
+++ functions/general/filesystem.sh
@@ -3,7 +3,11 @@
 # This file contains functions to work with files and folders
 ####################################################################################################
 
-source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
+fi
 prevent_to_execute_directly
 
 ####################################################################################################
