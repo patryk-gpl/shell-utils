c0108da727ab7404e50b2bea9a69a14d816a7325
Patryk <8299306+kpatryk@users.noreply.github.com>
2023-09-28 09:40:25 +0200

    feat(az): Add function to update all installed Azure CLI extensions

---
+++ functions/azure/az.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+####################################################################################################
+# This file contains functions to work with Azure CLI
+####################################################################################################
+
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
+fi
+
+prevent_to_execute_directly
+
+az_extension_update_all() {
+  az extension list --query "[].name" --output tsv | while read -r extension; do echo "Updating az extension $extension" ; az extension update --name "$extension" 2>&1 | grep -v "Use --debug for more information"; done
+}
