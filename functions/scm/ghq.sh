9ed32a2aa2676b85567387e6653fbd3cf383d292
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2023-10-27 14:48:37 +0200

    chore: Move all related Git functions to scm folder, update Git/Gcq functions

---
+++ functions/scm/ghq.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+####################################################################################################
+# This file contains functions to work with ghq tool
+####################################################################################################
+
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
+fi
+prevent_to_execute_directly
+
+
+function cdq() {
+    if [[ -z "$1" ]]; then
+        echo "Error: Please provide a directory name."
+        return 1
+    fi
+
+    local target_dir
+    target_dir="$(ghq list -p "$1")"
+
+    if [ -d "$target_dir" ]; then
+        cd "$target_dir" || return
+    else
+        echo "Error: Directory does not exist: $1"
+        return 1
+    fi
+}
+
+function ghq_tree() {
+  local root_dir="${1:-~/$HOME}"
+
+  if [ -d "$root_dir" ]; then
+    tree -d "$root_dir/github.com/" -L 2
+  else
+    echo "Directory $root_dir does not exist."
+  fi
+}
