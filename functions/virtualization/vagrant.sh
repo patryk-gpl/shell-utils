b393f3875c7b487f5cfade9b7b6cd995355df7b2
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2023-10-31 20:12:14 +0100

    feat(vagrant): Add alias to list available plugins

---
+++ functions/virtualization/vagrant.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+####################################################################################################
+# This file contains functions to work with Vagrant
+####################################################################################################
+
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
+fi
+prevent_to_execute_directly
+
+alias vagrant_list_avail_plugins="gem list --remote vagrant-"
