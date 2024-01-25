9a24227b70376eb18a30a11888df9a9f64c1ac87
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-01-25 09:17:06 +0100

    feat: Add script to read functions from shell-utils repo

---
+++ scripts/sync_shell_utils.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+git_repo_path="$(ghq list -p shell-utils)/functions"
+
+# shellcheck disable=SC1090,SC2044
+for file in $(find "$git_repo_path" -type f -name "*.sh"); do
+  source $file
+done
