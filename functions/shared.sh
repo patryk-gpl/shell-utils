a8134735bd3b00ae645d8a3e0a5f472ef78b05fa
Patryk <8299306+kpatryk@users.noreply.github.com>
2024-02-21 09:39:06 +0100

    feat: Filter all container logs of a pod with customer query value

--- functions/shared.sh
+++ functions/shared.sh
@@ -15,14 +15,14 @@ prevent_to_execute_directly() {
 
 # Colors ANSI escape codes
 RESET="\033[0m"
-black="\033[30m"
-red="\033[31m"
-green="\033[32m"
-yellow="\033[33m"
-blue="\033[34m"
-magenta="\033[35m"
-cyan="\033[36m"
-white="\033[37m"
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
 
 # Return the OS type
 find_os_type() {
