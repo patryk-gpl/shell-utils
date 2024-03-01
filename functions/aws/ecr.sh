8a66c46dc4e82aeb11ee12f5b378bc3cf346f83c
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-03-01 17:12:28 +0100

    feat(aws): Show AWS ECR repositories names by prefix

---
+++ functions/aws/ecr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+aws_ecr_get_repositories_by_prefix() {
+  local prefix="$1"
+
+  if [[ -z "$prefix" ]]; then
+    echo "Usage: aws_ecr_get_repositories_by_prefix <prefix>"
+    return 1
+  fi
+
+  aws ecr describe-repositories | jq -r '.repositories[] | select(.repositoryName | startswith("'"$prefix"'")) | .repositoryName' | sort
+}
