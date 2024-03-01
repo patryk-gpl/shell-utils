c5bc182f1afab8c39762c88d4d1a3351fd47a9ff
Patryk <8299306+kpatryk@users.noreply.github.com>
2024-03-01 17:12:28 +0100

    feat(aws): Show AWS ECR repositories names by prefix

--- functions/aws/iam.sh
+++ functions/aws/iam.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-####################################################################################################
 # This file contains functions to work with AWS IAM (Identity and Access Management)
-####################################################################################################
 
  function aws_get_caller_identity() {
    aws sts get-caller-identity | jq .
