d91331eeca063afd8a05f7a8c2b4471a411f5e3c
Patryk <8299306+kpatryk@users.noreply.github.com>
2024-03-01 18:56:32 +0100

    refactor(aws): Allow to provide a list of EC2 to stop / start / check status

--- functions/aws/ec2.sh
+++ functions/aws/ec2.sh
@@ -2,15 +2,39 @@
 # This file contains functions to work with AWS EC2
 
 aws_ec2_status() {
-  local instance_id="$1"
+  local instance_ids=("$@")
 
-  if [[ -z "$instance_id" ]]; then
-    echo "Usage: aws_ec2_status <instance_id>"
+  if [[ ${#instance_ids[@]} -eq 0 ]]; then
+    echo "Usage: aws_ec2_status <instance_id> [<instance_id> ...]"
     return 1
   fi
 
-  echo "State of the AWS EC2 instance $instance_id before reboot:"
-  aws ec2 describe-instances --instance-ids  $instance_id --query 'Reservations[].Instances[].{Name: Tags[?Key==`Name`].Value | [0], State: State.Name}' | jq .
+  echo "State of the AWS EC2 instances: ${instance_ids[@]}"
+  aws ec2 describe-instances --instance-ids "${instance_ids[@]}" --query 'Reservations[].Instances[].{Name: Tags[?Key==`Name`].Value | [0], State: State.Name}' --output table
+}
+
+aws_ec2_start(){
+  local instance_ids=("$@")
+
+  if [[ ${#instance_ids[@]} -eq 0 ]]; then
+    echo "Usage: aws_ec2_start <instance_id> [<instance_id> ...]"
+    return 1
+  fi
+
+  echo "Starting the AWS EC2 instances: ${instance_ids[@]}"
+  aws ec2 start-instances --instance-ids "${instance_ids[@]}" --output json | jq .
+}
+
+aws_ec2_stop(){
+  local instance_ids=("$@")
+
+  if [[ ${#instance_ids[@]} -eq 0 ]]; then
+    echo "Usage: aws_ec2_stop <instance_id> [<instance_id> ...]"
+    return 1
+  fi
+
+  echo "Stopping the AWS EC2 instances: ${instance_ids[@]}"
+  aws ec2 stop-instances --instance-ids "${instance_ids[@]}" --output json | jq .
 }
 
 aws_ec2_restart_instance() {
@@ -40,30 +64,6 @@ aws_ec2_restart_instance() {
     fi
     aws_ec2_start "$instance_id"
   fi
-}
-
-aws_ec2_start(){
-  local instance_id="$1"
-
-  if [[ -z "$instance_id" ]]; then
-    echo "Usage: aws_ec2_start <instance_id>"
-    return 1
-  fi
-
-  echo "Starting the AWS EC2 instance $instance_id..."
-  aws ec2 start-instances --instance-ids "$instance_id" | jq .
-}
-
-aws_ec2_stop(){
-  local instance_id="$1"
-
-  if [[ -z "$instance_id" ]]; then
-    echo "Usage: aws_ec2_stop <instance_id>"
-    return 1
-  fi
-
-  echo "Stopping the AWS EC2 instance $instance_id..."
-  aws ec2 stop-instances --instance-ids "$instance_id" | jq .
 }
 
 aws_ec2_get_instace_id_by_ip() {
