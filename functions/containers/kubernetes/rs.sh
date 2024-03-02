691718827229402e2dada296f659c9b45b59542d
Patryk <8299306+kpatryk@users.noreply.github.com>
2024-03-02 11:45:29 +0100

    chore: Remove headers

--- functions/containers/kubernetes/rs.sh
+++ functions/containers/kubernetes/rs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-####################################################################################################
 # This file contains functions to work with Kubernetes replica sets
-####################################################################################################
 
 if [[ -n "$ZSH_VERSION" ]]; then
   source "$(dirname "$0")/../../shared.sh"
