378fbad5b98193afeff4df46909007def08b0293
Patryk Kubiak <8299306+kpatryk@users.noreply.github.com>
2024-03-06 00:02:04 +0100

    feat(kubernetes): Check status of Metrics Server local API server, add minikube reset

---
+++ functions/containers/kubernetes/addons.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This file contains functions to work with Kubernetes cluster installed as addons
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../../shared.sh"
+fi
+prevent_to_execute_directly
+
+kube_metrics_server_api_raw_info() {
+  kubectl get --raw /apis/metrics.k8s.io/v1beta1/nodes | jq .
+}
