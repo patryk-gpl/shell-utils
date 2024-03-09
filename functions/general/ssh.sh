0c561874cfc4230d7740c57762e29aee6b5499f5
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-03-09 06:56:46 +0100

    feat(ssh): Add function to connect via SSH till successful

---
+++ functions/general/ssh.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This file contains functions to work with SSH
+
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
+fi
+prevent_to_execute_directly
+
+ssh_connect_with_retry() {
+  local host="$1"
+
+  if [[ -z "$host" ]]; then
+    echo "Error: Host not provided."
+    return 1
+  fi
+
+  echo "Connecting to $host with auto-retry (sleep 5 seconds).."
+  while ! ssh "$host"; do sleep 5; done
+}
