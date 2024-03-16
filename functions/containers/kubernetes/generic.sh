56e0ab03dd99310c04f3abcf1ea487b085dd6aec
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-03-16 08:18:27 +0100

    chore: Make description header shorter

--- functions/containers/kubernetes/generic.sh
+++ functions/containers/kubernetes/generic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This file contains functions to work with Kubernetes (generic)
+# Functions to work with Kubernetes (generic)
 
 if [[ -n "$ZSH_VERSION" ]]; then
   source "$(dirname "$0")/../../shared.sh"
