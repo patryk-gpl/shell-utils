cf8b74c09663c9da051a4c37c9fa92544a3beb5d
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-03-16 08:18:27 +0100

    chore: Make description header shorter

--- functions/scm/git/log.sh
+++ functions/scm/git/log.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This file contains functions to work with Git log
+# Functions to work with Git log
 if [[ -n "$ZSH_VERSION" ]]; then
   source "$(dirname "$0")/../../shared.sh"
 else
--- functions/security/trivy.sh
+++ functions/security/trivy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This file contains functions to work with trivy scanner
+# Functions to work with trivy scanner
 
 if [[ -n "$ZSH_VERSION" ]]; then
   source "$(dirname "$0")/../shared.sh"
