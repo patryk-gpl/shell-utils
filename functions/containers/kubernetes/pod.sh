c93b4fd04f11448d566ed45694a1b33960502a9c
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-05-08 15:32:33 +0200

    refactor: Add functions to dump logs from pods, and all containers (including init)

--- functions/containers/kubernetes/pod.sh
+++ functions/containers/kubernetes/pod.sh
@@ -13,25 +13,6 @@ kube_list_image_names_from_pods() {
   kubectl get pods -n "$namespace" -o=jsonpath="{range .items[*].spec.containers[*]}{.image}{'\n'}{end}" "$@" | sort -u
 }
 
-kube_pod_get_logs_from_pod_containers_with_filter() {
-  local pod_name="$1"
-  local namespace="$2"
-  local filter="$3"
-
-  if [[ -z "$pod_name" || -z "$namespace" || -z "$filter" ]]; then
-    echo "Usage: ${FUNCNAME[0]} <pod_name> <namespace> <filter>"
-    return 1
-  fi
-
-  local containers
-  containers=$(kubectl get pod "$pod_name" -n "$namespace" -o=jsonpath='{.spec.containers[*].name}')
-
-  for container in $containers; do
-    echo -e "${GREEN}== Logs for container $container in pod $pod_name ==${RESET}"
-    kubectl logs -n "$namespace" "$pod_name" -c "$container" | grep -E -i "$filter"
-  done
-}
-
 function kube_pods_describe_all() {
   namespace=$1
   if [ -z "$namespace" ]; then
@@ -47,17 +28,6 @@ function kube_pods_describe_all() {
     echo "Describe pod $pod in namespace $namespace"
     echo "========================================" >>"$filename"
     kubectl describe pod "$pod" -n "$namespace" >>"$filename"
-  done
-}
-
-kube_pods_dump_all_logs() {
-  local pods
-  local namespace="$1"
-  pods=$(kubectl get pods -n "$namespace" --no-headers -o custom-columns=":metadata.name")
-
-  for pod in $pods; do
-    kubectl logs -n "$namespace" "$pod" >"${pod}.log"
-    echo "Logs for pod $pod dumped to ${pod}.log"
   done
 }
 
