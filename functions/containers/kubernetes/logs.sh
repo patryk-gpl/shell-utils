e406d62ff0d7f83a19d7760aaff03289a6c72f97
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-05-08 15:32:33 +0200

    refactor: Add functions to dump logs from pods, and all containers (including init)

---
+++ functions/containers/kubernetes/logs.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Functions to work with Kubernetes logs
+
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../../shared.sh"
+fi
+prevent_to_execute_directly
+
+kube_logs_dump_from_pod_containers_with_filter() {
+  local namespace="$1"
+  local pod_name="$2"
+  local filter="$3"
+
+  if [[ -z "$pod_name" || -z "$namespace" || -z "$filter" ]]; then
+    echo "Usage: ${FUNCNAME[0]} <namespace> <pod_name> <filter>"
+    return 1
+  fi
+
+  local containers
+  containers=$(kubectl get pod "$pod_name" -n "$namespace" -o=jsonpath='{.spec.containers[*].name}')
+
+  for container in $containers; do
+    echo -e "${GREEN}== Logs for container $container in pod $pod_name ==${RESET}"
+    kubectl logs -n "$namespace" "$pod_name" -c "$container" | grep -E -i "$filter"
+  done
+}
+
+kube_logs_dump_from_all_pods() {
+  local namespace="$1"
+
+  if [[ -z "$namespace" ]]; then
+    echo "Usage: ${FUNCNAME[0]} <namespace>"
+    return 1
+  fi
+
+  local pods
+  pods=$(kubectl get pods -n "$namespace" --no-headers -o custom-columns=":metadata.name")
+
+  for pod in $pods;
+  do
+    echo "Dumping logs for pod $pod, namespace $namespace..."
+    containers=$(kubectl get pod "$pod" -n "$namespace" -o jsonpath='{.spec.containers[*].name} {.spec.initContainers[*].name}')
+    for container in $containers
+    do
+      kubectl logs -n "$namespace" "$pod" -c "$container" > "$namespace_$pod_${container}.log"
+    done
+  done
+}
