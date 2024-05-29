a67ff9828a9d4f19994502db25d1e632d15c875f
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-05-29 07:51:20 +0200

    fix: Resolving issue 'not following: ./../shared.sh: openBinaryFile: does not exist (No such file or directory) [SC1091]'

--- scripts/sync_shell_utils.sh
+++ scripts/sync_shell_utils.sh
@@ -4,5 +4,5 @@ git_repo_path="$(ghq list -p shell-utils)/functions"
 
 # shellcheck disable=SC1090,SC2044
 for file in $(find "$git_repo_path" -type f -name "*.sh"); do
-  source $file
+  source "$file"
 done
