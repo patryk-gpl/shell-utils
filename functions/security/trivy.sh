baaa9b857bc9220925bd43b38aca24339f900603
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-05-29 16:05:01 +0200

    refactor: Update scripts to handle SSL/TLS certs

--- functions/security/trivy.sh
+++ functions/security/trivy.sh
@@ -10,12 +10,11 @@ prevent_to_execute_directly
 
 trivy_scan_local_docker_images() {
   is_installed trivy || return 1
-  prefix=${1:-}
+  local prefix=${1:-}
   if [[ -z "$prefix" ]]; then
     echo "Usage: ${FUNCNAME[0]} <image_prefix>"
     return 1
   fi
-
   docker images --format "{{.Repository}}:{{.Tag}}" |
     grep "^$prefix" |
     xargs -I {} bash -c 'echo "Scanning image: {}"; trivy image --scanners misconfig {}'
