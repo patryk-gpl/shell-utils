2cc17a65e4014bb18cd7e09e49db542d78090d72
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-06-15 08:38:23 +0200

    feat(kube): Add function to cleanup completed jobs

---
+++ functions/containers/kubernetes/jobs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Functions to work with Kubernetes (jobs)
+
+if [[ -n "$ZSH_VERSION" ]]; then
+  source "$(dirname "$0")/../../shared.sh"
+else
+  source "$(dirname "${BASH_SOURCE[0]}")/../../shared.sh"
+fi
+prevent_to_execute_directly
+
+kube_jobs_remove_completed() {
+  local namespace="$1"
+  if [ -z "$namespace" ]; then
+    namespace=$(kubectl config view --minify --output 'jsonpath={..namespace}')
+    echo "No namespace provided, using current context namespace: $namespace"
+  else
+    echo "Using namespace: $namespace"
+
+  fi
+  jobs=$(kubectl get job -o=jsonpath='{.items[?(@.status.succeeded==1)].metadata.name}' -n "$namespace")
+  if [ -z "$jobs" ]; then
+    echo "No completed jobs found in namespace: $namespace. Skipping..."
+    return 0
+  else
+    echo "Removing completed jobs in namespace: $namespace"
+    kubectl delete job "$jobs" -n "$namespace"
+  fi
+}
