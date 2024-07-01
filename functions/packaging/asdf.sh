1bd7a2284d7862adc76fc57875f5ce6124899261
Patryk <8299306+kpatryk@users.noreply.github.com>
2024-07-01 13:07:37 +0700

    chore(asdf): Update function to show installation script of the plugin

--- functions/packaging/asdf.sh
+++ functions/packaging/asdf.sh
@@ -9,7 +9,19 @@ fi
 prevent_to_execute_directly
 
 function asdf_plugin_show_install_script() {
+  if ! command -v asdf &>/dev/null; then
+    echo "Error: asdf is not installed."
+    return 1
+  fi
+
   local plugin_name=$1
+  if [[ -z $plugin_name ]]; then
+    echo "Error: Plugin name is missing."
+    echo "Available installed plugins:"
+    asdf plugin list
+    return 1
+  fi
+
   local bin_path="$HOME/.asdf/plugins/$plugin_name/bin/install"
 
   if [[ -f $bin_path ]]; then
