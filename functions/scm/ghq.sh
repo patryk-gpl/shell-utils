fc31c37ad2a1224a25584e7073f5a2b33880226a
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-07-08 13:28:50 +0700

    refactor: Update cdq to work in both Bash and ZSH, add option to select from multiple items if more than 1 matching folder is found

--- functions/scm/ghq.sh
+++ functions/scm/ghq.sh
@@ -14,14 +14,48 @@ cdq() {
     return 1
   fi
 
-  local target_dir
-  target_dir="$(ghq list -p "$1")"
+  local dirs
+  dirs=$(ghq list -p "$1")
 
-  if [ -d "$target_dir" ]; then
-    cd "$target_dir" || return
-  else
-    echo "Error: Directory does not exist: $1"
+  if [[ -z "$dirs" ]]; then
+    echo "Error: No matching directories found for: $1"
     return 1
+  fi
+
+  if [[ -n $ZSH_VERSION ]]; then
+    # shellcheck disable=SC2296
+    local target_dirs=("${(@f)dirs}")
+  else
+    # shellcheck disable=SC2206
+    local target_dirs=($dirs)
+  fi
+
+  if [ ${#target_dirs[@]} -eq 1 ]; then
+    # shellcheck disable=SC2124
+    local dir="${target_dirs[@]:0:1}"
+    cd "$dir"
+  else
+    echo "Multiple matching directories found:"
+    local i=1
+    for dir in "${target_dirs[@]}"; do
+      echo "$i: $dir"
+      ((i++))
+    done
+
+    local selection
+    echo -n "Select a directory (1-${#target_dirs[@]}): "
+    read -r selection
+
+    selection=$(echo "$selection" | tr -cd '[:digit:]')
+
+    if [[ ! "$selection" =~ ^[0-9]+$ ]] || [ "$selection" -lt 1 ] || [ "$selection" -gt "${#target_dirs[@]}" ]; then
+      echo "Error: Invalid selection."
+      return 1
+    fi
+
+    # shellcheck disable=SC2124
+    local selected_dir="${target_dirs[@]:$((selection-1)):1}"
+    cd "$selected_dir" && echo "Changed to directory: $selected_dir"
   fi
 }
 
