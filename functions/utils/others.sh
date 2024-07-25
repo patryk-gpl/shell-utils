46ef92ad9c356626d62972bdd56b41fb47af8d5e
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-07-25 16:02:06 +0200

    feat(extract): Add function to extract block of text from the file

--- functions/utils/others.sh
+++ functions/utils/others.sh
@@ -47,3 +47,38 @@ extract_function() {
   }
   ' "$filename"
 }
+
+# Function: extract_block
+# Description: Extracts a block of text from a file based on start and end patterns.
+# Parameters:
+#   - file: The file to extract the block from.
+#   - start_pattern: The pattern that marks the start of the block.
+#   - end_pattern: The pattern that marks the end of the block.
+# Usage: extract_block <file> <start_pattern> <end_pattern>
+# Example: extract_block file.yaml '# Source: release/charts/opensearch/templates/poddisruptionbudget.yaml' '^---'
+# Returns:
+#   - 0: If the block is successfully extracted.
+#   - 1: If there is an error or the block cannot be found.
+extract_block() {
+  if [ $# -ne 3 ]; then
+    echo "Usage: extract_block <file> <start_pattern> <end_pattern>" >&2
+    echo "Example: extract_block file.yaml '# Source: netreveal/charts/opensearch/templates/poddisruptionbudget.yaml' '^---'" >&2
+    return 1
+  fi
+
+  local file="$1"
+  local start_pattern="$2"
+  local end_pattern="$3"
+
+  if [ ! -f "$file" ]; then
+    echo "Error: File '$file' not found." >&2
+    return 1
+  fi
+
+  awk -v start="$start_pattern" -v end="$end_pattern" '
+  BEGIN { printing = 0; }
+  $0 ~ start { printing = 1; next; }
+  $0 ~ end { if (printing) exit; }
+  printing == 1 { print; }
+  ' "$file"
+}
