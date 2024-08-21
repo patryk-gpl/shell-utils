dd43b593985c7014aa5a3ecb2a8abce1632bda71
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-08-21 13:18:34 +0200

    feat: Update sync shell scripts to use symlinks

--- scripts/sync_shell_utils.sh
+++ scripts/sync_shell_utils.sh
@@ -1,8 +1,35 @@
 #!/usr/bin/env bash
 
-git_repo_path="$(ghq list -p shell-utils)/functions"
+resolve_script_path() {
+    local source=$1
+    local dir
+
+    while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+        dir="$( cd -P "$( dirname "$source" )" &> /dev/null && pwd )"
+        source="$(readlink "$source")"
+        [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    dir="$( cd -P "$( dirname "$source" )" &> /dev/null && pwd )"
+    echo "$dir"
+}
+
+# Get the real directory of the script, compatible with Bash and Zsh, handling symlinks
+if [[ -n $BASH_VERSION ]]; then
+    script_dir=$(resolve_script_path "${BASH_SOURCE[0]}")
+elif [[ -n $ZSH_VERSION ]]; then
+    script_dir=$(resolve_script_path "${(%):-%N}")
+else
+    script_dir=$(resolve_script_path "$0")
+fi
+
+functions_dir="$script_dir/../functions"
+
+if [ ! -d "$functions_dir" ]; then
+    echo "Error: Directory $functions_dir does not exist."
+    exit 1
+fi
 
 while IFS= read -r -d '' file; do
-  # shellcheck disable=SC1090
-  source "$file"
-done < <(find "$git_repo_path" -type f -name "*.sh" -print0)
+    # shellcheck disable=SC1090
+    source "$file"
+done < <(find "$functions_dir" -type f -name "*.sh" -print0)
