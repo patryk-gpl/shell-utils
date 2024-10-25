6accef58ee92f47bc8cab531216878654d917b56
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-10-25 18:02:10 +0200

    chore: Remove prevent block from scripts

--- functions/containers/flux.sh
+++ functions/containers/flux.sh
@@ -1,12 +1,5 @@
 # Functions to work with Flux v2
 
-if [[ -n "$ZSH_VERSION" ]]; then
-  source "$(dirname "$0")/../shared.sh"
-else
-  source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
-fi
-prevent_to_execute_directly
-
 flux_list_all_active_resources() {
   local flux_resources
   local namespace="$1"
--- functions/shell/ssh.sh
+++ functions/shell/ssh.sh
@@ -1,10 +1,3 @@
-if [[ -n "$ZSH_VERSION" ]]; then
-  source "$(dirname "$0")/../shared.sh"
-else
-  source "$(dirname "${BASH_SOURCE[0]}")/../shared.sh"
-fi
-prevent_to_execute_directly
-
 ssh_connect_with_retry() {
   local host="$1"
   local timeout=5
