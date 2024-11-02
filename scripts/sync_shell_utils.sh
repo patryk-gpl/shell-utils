f50c5e304534305b828879d0753ef603b882e20a
Patryk Kubiak <Patryk.Kubiak@gmail.com>
2024-11-02 12:23:16 +0100

    feat: Add PF rules to manage firewall

--- scripts/sync_shell_utils.sh
+++ scripts/sync_shell_utils.sh
@@ -23,21 +23,33 @@ else
 fi
 
 functions_dir="$script_dir/../functions"
+functions_mac_dir="$script_dir/../functions_mac"
 
 if [ ! -d "$functions_dir" ]; then
-    echo "Error: Directory $functions_dir does not exist."
-    exit 1
+  echo "Error: Directory $functions_dir does not exist."
+  return 1
 fi
 
 if [ -f "$functions_dir/shared.sh" ]; then
-    # shellcheck disable=SC1091
-    source "$functions_dir/shared.sh"
+  # shellcheck disable=SC1091
+  source "$functions_dir/shared.sh"
 else
-    echo "$functions_dir/shared.sh not found."
-    exit 1
+  echo "$functions_dir/shared.sh not found."
+  return 1
 fi
 
 while IFS= read -r -d '' file; do
-    # shellcheck disable=SC1090
-    source "$file"
+  # shellcheck disable=SC1090
+  source "$file"
 done < <(find "$functions_dir" -type f -name "*.sh" -print0)
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if [ -d "$functions_mac_dir" ]; then
+    while IFS= read -r -d '' file; do
+      # shellcheck disable=SC1090
+      source "$file"
+    done < <(find "$functions_mac_dir" -type f -name "*.sh" -print0)
+  else
+    echo "Directory $functions_mac_dir does not exist, skipping Mac-specific scripts."
+  fi
+fi
